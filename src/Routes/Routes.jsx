e30dc76d2cab81146110f1a9584a44f5dc053d3d
instasif <asifh9277@gmail.com>
2024-09-21 02:12:00 +0600

    made dashboard private route

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -7,6 +7,7 @@ import Login from "../Pages/Login/Login";
 import SignUp from "../Pages/SignUp/SignUp";
 import Dashboard from "../Layout/Dashboard";
 import Cart from "../Pages/Dashboard/Cart/Cart";
+import PrivateRoute from "./PrivateRoute";
 
 export const router = createBrowserRouter([
   {
@@ -41,7 +42,7 @@ export const router = createBrowserRouter([
     children: [
       {
         path: "cart",
-        element: <Cart></Cart>,
+        element: <PrivateRoute><Cart /></PrivateRoute>,
       },
     ],
   },
